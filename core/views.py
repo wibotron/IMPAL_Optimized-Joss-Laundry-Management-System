691ad87c3156b041c60f481ab8f6f7d05ae62036
MZeidane123 <mochzeidane@gmail.com>
2026-06-08 22:26:25 +0700

    commit change di views.py
    penambahan beberapa path

--- core/views.py
+++ core/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 def landing_page(request):
     return render(request, 'landing.html')
+
+def about_page(request):
+    return render(request, 'about.html')
+
+def contact_page(request):
+    return render(request, 'contact.html')
+
+def faq_page(request):
+    return render(request, 'faq.html')
+
+def terms_page(request):
+    return render(request, 'terms.html')
+
+def privacy_page(request):
+    return render(request, 'privacy.html')
